20bdc4ef8ca939f5c3fd7dd0cc1322f6f4d9257e
Ernie Casilla <ecasilla@icloud.com>
2014-10-30 03:08:37 -0300

    working the child process spawning for starting the server

--- test/helpers/periodic_instance.js
+++ test/helpers/periodic_instance.js
@@ -1,6 +1,8 @@
-var npm     = require('npm'),
-    path    = require('path'),
-    fs      = require('fs-extra');
+var npm      = require('npm'),
+    path     = require('path'),
+    fs       = require('fs-extra'),
+    child    = require('child_process'),
+    periodic_cwd = path.resolve(previous_dir(),'node_modules/periodicjs/'),
     periodic = "periodicjs@latest";
 
 create_previous_dirSync(previous_dir())
@@ -13,7 +15,8 @@ npm.load({prefix:previous_dir()}, function (err) {
    if (err) {
     throw err 
    } 
-   console.log(data);
+   start_server();
+   
   });
   npm.on("log", function (message) {
     // log the progress of the installation
@@ -22,8 +25,17 @@ npm.load({prefix:previous_dir()}, function (err) {
 });
 
 
+function start_server() {
+ var server = child.exec('npm run nd',{cwd:periodic_cwd},function(error,stdout,stderr) { 
+    if (error) {
+     throw error; 
+    }
+    console.log(stdout);
+ }); 
+}
+
 function previous_dir() {
- return path.resolve(process.cwd(),'../periodic_Stub');
+ return path.resolve(process.cwd(),'../periodicjs_Stub');
 }
 function create_previous_dirSync(path_to) {
  return fs.mkdirsSync(path_to); 
@@ -31,6 +43,12 @@ function create_previous_dirSync(path_to) {
 function remove_previous_dirSync() {
   return fs.removeSync(previous_dir());
 }
+function kill_server(server) {
+  psTree(server.pid, function (err, children) {
+    cp.spawn('kill', ['-9'].concat(children.map(function (p) {return p.PID})))
+  }) 
+}
 module.exports.previous_dir = previous_dir;
 module.exports.create_previous_dir = create_previous_dirSync;
 module.exports.remove_previous_dirSync = remove_previous_dirSync;
+module.exports.kill_server = kill_server;
--- test/helpers/periodic_instance_spec.js
+++ test/helpers/periodic_instance_spec.js
@@ -1,20 +1,23 @@
 var expect       = require('chai').expect,
     path         = require('path'),
-    exec = require('child_process').exec,
+    chai         = require('chai'),
+    child        = require('child_process'),
+    nexpect      = require('nexpect'),
     previous_dir = require('./periodic_instance').previous_dir,
-    remove_dir = require('./periodic_instance').remove_previous_dirSync,
-    context = describe;
+    remove_dir   = require('./periodic_instance').remove_previous_dirSync,
+    stubCWD      = path.resolve(previous_dir(),'periodic_Stub/node_modules/periodicjs/');
+    context      = describe;
     chai.use(require('chai-fs'))
 
 describe('The periodic Stub', function(){
 
   context('folder management of for the install process',function() {
     it('should have a stub folder in the previous directory', function(done){
-      expect(previous_dir()).to.have.dirname('periodic_Stub')
+      expect(previous_dir()).to.be.a.directory('periodic_Stub')
       done();
     });
     it('should create a folder in the previous dir to install the instance', function(done){
-      expect(previous_dir()).to.be.a.directory('periodic_Stub').and.empty;
+      expect(previous_dir()).to.be.a.directory('periodic_Stub/node_modules/periodicjs');
       done(); 
     });
   })
@@ -22,34 +25,38 @@ describe('The periodic Stub', function(){
   context('The npm install process',function() {
     this.timeout(9000);
     before(function(done){
-      var install = 'node' + path.resolve(process.cwd(),'test/helpers/periodic_instance.js');
-      exec(install,function(error,stdout,stderr) {
-        if (error)  done(error) 
-       capturedOut = stdout 
+      var install = 'node ' + path.resolve(process.cwd(),'test/helpers/periodic_instance.js');
+      child.exec(install,function(error,stdout,stderr) {
+        if (error)  done(error);
+        process.stdout.write(stdout);
       })
       done();
     });
     after(function(done){
-     remove_dir(previous_dir()) 
+     remove_dir(previous_dir());
      done();
     });
 
     it('should install the latest periodic', function(done){
-      var packageJson = path.resolve(previous_dir(),'package.json')
-      expect(packageJson).to.be.a.file().with.json;
+      var periodic_direc = path.resolve(previous_dir(),'node_modules/periodicjs/');
+      expect(periodic_direc).to.be.a.directory();
       done()
     });
     it('should match the package.json name', function(done){
-      var packageJson = path.resolve(previous_dir(),'package.json')
-      expect(packageJson).to.have.content.that.match(/periodicjs/)
+      var packageJson = path.resolve(previous_dir(),'node_modules/periodicjs/package.json');
+      expect(packageJson).to.have.content.that.match(/periodicjs/);
       done()
     });
     it('should start the server on the periodic folder', function(done){
-      expect()
-      done()
-    });
-    it('should stop the serve once the test have completed', function(done){
-      expect()
+      var server = child.spawn('npm', ['run',' nd'],{stdio:'inherit',cwd:stubCWD})
+      server.on('error',function(err) {
+       console.log('Encountered error:', err);
+       process.exit();
+      })
+      server.on('close',function() {
+        console.log('closed server: ' + server.pid );
+      })
+      expect(server).to.be.ok
       done()
     });
   })
